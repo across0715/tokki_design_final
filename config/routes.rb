823e57292b15adabbfb1c5c73342453e59397fe2
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-11-11 15:05:36 +0900

    root設定、ビューファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get  'static_pages/about'
-  root 'static_pages#home'
+  root to: "static_pages#home"
+  get '/home',        to: 'static_pages#home'
+  get '/about',       to: 'static_pages#about'
+  get '/gallery',     to: 'static_pages#gallery'
+  get '/faq',        to: 'static_pages#faq'
+
+  
+  
 end
