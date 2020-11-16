4cbb25af92ceba50d27731c43ebf04ac459ad381
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-11-17 08:40:56 +0900

    migrating up

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_16_233209) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
