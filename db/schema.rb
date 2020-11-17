982d13746df94ac2a44378b085ca070351a2dea3
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-11-17 10:07:24 +0900

    password_digestカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_004312) do
+ActiveRecord::Schema.define(version: 2020_11_17_010455) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_004312) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
