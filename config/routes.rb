4e63b9ea3d9eda2c3f157d2669e4c672a063430c
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-11-25 08:13:32 +0900

    sessionリソースを追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   get '/faq',         to: 'static_pages#faq'
 
   get  '/signup',     to: 'users#new'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   resources :users
   
 end
