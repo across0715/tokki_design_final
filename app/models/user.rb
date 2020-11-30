bd826e034dc5b21b596e083baa910e5d11f29c1a
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-11-30 13:11:39 +0900

    authenticated?を更新して、ダイジェストが存在しない場合に対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@ class User < ApplicationRecord
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
@@ -36,5 +37,5 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
-  
+
 end
