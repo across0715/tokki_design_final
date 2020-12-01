f31288c9f4366982311b4111328cbe146b9a7a32
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-12-01 14:25:01 +0900

    Usersをページネート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
-    @users = User.paginate(page: params[:page])
-  end
+    @user = User.find(params[:id])
     
+  end
   
   def new
     @user = User.new
