1658a9b42cc9e4d6b7ebaee9e7d8cd45316ae4af
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-12-01 14:13:21 +0900

    will_paginateを追加

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_11_25_043414) do
 
-  create_table "posts", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
