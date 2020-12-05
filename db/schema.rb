71a299c4711f76ffd626b249ec614f7aa729d160
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-12-05 12:22:21 +0900

    index整理

--- db/schema.rb
+++ db/schema.rb
@@ -13,17 +13,17 @@
 ActiveRecord::Schema.define(version: 2020_12_05_032011) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.boolean "admin", default: false
-    t.string "activation_digest"
-    t.boolean "activated", default: false
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "admin", default: false
+    t.string   "activation_digest"
+    t.boolean  "activated", default: false
     t.datetime "activated_at"
-    t.string "reset_digest"
+    t.string   "reset_digest"
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
