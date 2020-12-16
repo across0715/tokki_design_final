9a4a02eb43017fb0ba78dd1b2ad729876cc643f3
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-12-16 09:55:41 +0900

    finish ch12

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -37,9 +37,9 @@ class PasswordResetsController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:password, :password_confirmation)
-    end
+  def user_params
+  params.require(:user).permit(:password, :password_confirmation)
+  end
 
     # beforeフィルタ
 
@@ -62,3 +62,6 @@ class PasswordResetsController < ApplicationController
         redirect_to new_password_reset_url
       end
     end
+
+  end
+  
