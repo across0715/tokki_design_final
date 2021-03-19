ff04c9839d032908f1dbb251ed9ace85ce50000a
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-03-19 18:29:37 +0900

    micropost 投稿フォーム作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
-
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def about
@@ -8,8 +8,7 @@ class StaticPagesController < ApplicationController
 
   def gallery
   end
-  
+
   def faq
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
-
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  get 'sessions/new'
+  get "password_resets/new"
+  get "password_resets/edit"
+  get "sessions/new"
   root to: "static_pages#home"
-  get '/home',        to: 'static_pages#home'
-  get '/about',       to: 'static_pages#about'
-  get '/gallery',     to: 'static_pages#gallery'
-  get '/faq',         to: 'static_pages#faq'
+  get "/home", to: "static_pages#home"
+  get "/about", to: "static_pages#about"
+  get "/gallery", to: "static_pages#gallery"
+  get "/faq", to: "static_pages#faq"
 
-  get  '/signup',     to: 'users#new'
+  get "/signup", to: "users#new"
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   resources :users
   resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
 end
