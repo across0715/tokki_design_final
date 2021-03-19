a919afa5062c21efbe7e55fb2d8c1e0165e6b266
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-03-19 19:12:32 +0900

    list 13.54

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,12 @@
-require 'test_helper'
+require "test_helper"
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference "Micropost.count" do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
+  end
 end
