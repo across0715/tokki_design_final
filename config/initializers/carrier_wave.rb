d6f3d4f40449d027d6dee74b06c9e0903b5ccef3
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-03-20 09:56:14 +0900

    carrier_wave に追記

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,11 +3,11 @@ if Rails.env.production?
     config.fog_provider = "fog/aws" #←ここを追記
     config.fog_credentials = {
       :provider => "AWS",
-      :region => ENV["S3_region=ap-northeast-1"],
-      :aws_access_key_id => ENV["S3_AKIAX4AEDLC2O3Y475JN"],
-      :aws_secret_access_key => ENV["S3_RRxeUtCoM2J5I3QIZ0x28T2nkimcqRWY4JkANiKS"],
+      :region => ENV["region=ap-northeast-1"],
+      :aws_access_key_id => ENV["AKIAX4AEDLC2O3Y475JN"],
+      :aws_secret_access_key => ENV["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"],
     }
-    config.fog_directory = ENV["S3_tokki-design"]
+    config.fog_directory = ENV["tokki-design"]
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
 
