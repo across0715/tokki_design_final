3d926b4583bed4dac0f2e4c44953de4e91a073b6
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-04-01 13:10:41 +0900

    アマゾンS3キー再取得し変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,48 +1,50 @@
-config.cache_classes = true
+Rails.application.configure do
+  config.cache_classes = true
 
-config.eager_load = true
+  config.eager_load = true
 
-config.consider_all_requests_local = false
-config.action_controller.perform_caching = true
-config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-config.assets.compile = false
+  config.assets.compile = false
 
-config.active_storage.service = :local
+  config.active_storage.service = :local
 
-config.force_ssl = true
+  config.force_ssl = true
 
-config.log_level = :debug
+  config.log_level = :debug
 
-config.log_tags = [:request_id]
+  config.log_tags = [:request_id]
 
-config.active_storage.service = :amazon
+  config.active_storage.service = :amazon
 
-config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = false
 
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
-host = "tokkidesign.herokuapp.com"
-config.action_mailer.default_url_options = { host: host }
-ActionMailer::Base.smtp_settings = {
-  :port => ENV["MAILGUN_SMTP_PORT"],
-  :address => ENV["MAILGUN_SMTP_SERVER"],
-  :user_name => ENV["MAILGUN_SMTP_LOGIN"],
-  :password => ENV["MAILGUN_SMTP_PASSWORD"],
-  :domain => host,
-  :authentication => :plain,
-}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = "tokkidesign.herokuapp.com"
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :port => ENV["MAILGUN_SMTP_PORT"],
+    :address => ENV["MAILGUN_SMTP_SERVER"],
+    :user_name => ENV["MAILGUN_SMTP_LOGIN"],
+    :password => ENV["MAILGUN_SMTP_PASSWORD"],
+    :domain => host,
+    :authentication => :plain,
+  }
 
-config.i18n.fallbacks = true
+  config.i18n.fallbacks = true
 
-config.active_support.deprecation = :notify
+  config.active_support.deprecation = :notify
 
-config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = ::Logger::Formatter.new
 
-if ENV["RAILS_LOG_TO_STDOUT"].present?
-  logger = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  config.active_record.dump_schema_after_migration = false
 end
-
-config.active_record.dump_schema_after_migration = false
