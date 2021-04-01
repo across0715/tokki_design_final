7aa4383b8ff90da86f22285d19385f280121febe
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-04-01 15:18:44 +0900

    config.active_storage.service = :local コメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
 
   config.assets.compile = false
 
-  config.active_storage.service = :local
+  #config.active_storage.service = :local
 
   config.force_ssl = true
 
