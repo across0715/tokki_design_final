45fee06fb587573b52eabf6049dc007ed322bbed
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-04-02 15:13:52 +0900

    フィードの最終的な実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -81,10 +81,12 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
-                    following_ids: following_ids, user_id: id)
-  end
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
 
   # ユーザーをフォローする
   def follow(other_user)
