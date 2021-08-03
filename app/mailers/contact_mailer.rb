e18a42f817e52ca413e6932eb895b6857d569aef
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-08-03 09:00:44 +0900

    やんばる2.5 メールの送信

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,23 +1,12 @@
 class ContactMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_mailer.user_email.subject
-  #
   def user_email(name:, email:)
     @name = name
     mail(
       to: email,
-      subject: "【tokki_design】Your inquiry has been received.",
+      subject: "【Tokki Design】Your inquiry has been received.",
     )
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_mailer.admin_email.subject
-  #
   def admin_email
   end
 end
