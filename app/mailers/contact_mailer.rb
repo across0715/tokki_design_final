193480afa1c70041163f21ac45b1ec2352a185c8
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-08-12 08:35:15 +0900

    サンプルメールの送信準備

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,19 @@
 class ContactMailer < ApplicationMailer
-  def user_email(name:, email:)
-    @name = name
-    mail(
-      to: email,
-      subject: "【Tokki Design】Your inquiry has been received.",
-    )
+  ADMIN_EMAIL = "tokki2tokki@gmail.com"
+
+  def user_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = "【Tokki Design】 Your inquiry has been received."
+
+    mail(to: contact.email, subject: subject)
   end
 
-  def admin_email
+  def admin_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = "【Tokki Design】 You have an inquiry."
+
+    mail(to: ADMIN_EMAIL, subject: subject)
   end
 end
