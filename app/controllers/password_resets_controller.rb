252ffb9ecf7ded0b47358e58c6846f9b743dd1c1
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2021-09-09 08:08:15 +0900

    パスワードの再設定

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,5 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user,   only: [:edit, :update]
+  before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
   before_action :check_expiration, only: [:edit, :update]    # （1）への対応
 
@@ -15,53 +15,64 @@ class PasswordResetsController < ApplicationController
       redirect_to root_url
     else
       flash.now[:danger] = "Email address not found"
-      render 'new'
+      render "new"
     end
   end
 
   def edit
   end
 
+  #def update
+  #  if params[:user][:password].empty?                  # （3）への対応
+  #    @user.errors.add(:password, :blank)
+  #    render 'edit'
+  #  elsif @user.update(user_params)                     # （4）への対応
+  #    log_in @user
+  #    flash[:success] = "Password has been reset."
+  #    redirect_to @user
+  #  else
+  #    render 'edit'                                     # （2）への対応
+  #  end
+  #end
+
   def update
-    if params[:user][:password].empty?                  # （3）への対応
+    if params[:user][:password].empty? # (3) への対応
       @user.errors.add(:password, :blank)
-      render 'edit'
-    elsif @user.update(user_params)                     # （4）への対応
+      render "edit"
+    elsif @user.update_attributes(user_params) # (4) への対応
       log_in @user
       flash[:success] = "Password has been reset."
       redirect_to @user
     else
-      render 'edit'                                     # （2）への対応
+      render "edit"                                     # (2) への対応
     end
   end
 
   private
 
   def user_params
-  params.require(:user).permit(:password, :password_confirmation)
+    params.require(:user).permit(:password, :password_confirmation)
   end
 
-    # beforeフィルタ
+  # beforeフィルタ
 
-    def get_user
-      @user = User.find_by(email: params[:email])
-    end
-
-    # 有効なユーザーかどうか確認する
-    def valid_user
-      unless (@user && @user.activated? &&
-              @user.authenticated?(:reset, params[:id]))
-        redirect_to root_url
-      end
-    end
-
-    # トークンが期限切れかどうか確認する
-    def check_expiration
-      if @user.password_reset_expired?
-        flash[:danger] = "Password reset has expired."
-        redirect_to new_password_reset_url
-      end
-    end
-
+  def get_user
+    @user = User.find_by(email: params[:email])
   end
-  
+
+  # 有効なユーザーかどうか確認する
+  def valid_user
+    unless (@user && @user.activated? &&
+            @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
+
+  # トークンが期限切れかどうか確認する
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
+  end
+end
